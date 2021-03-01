ee36c16d1e4afac6b7e8d817dcb9f6f4c294b151
Lauren Dorval <dorvallg@dukes.jmu.edu>
2021-03-01 13:17:11 -0500

    hha

--- make.sql
+++ make.sql
@@ -4,5 +4,3 @@ DROP USER IF EXISTS feed_user@localhost;
 CREATE DATABASE comments CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER feed_user@localhost IDENTIFIED BY 'Laur3nMom$Bl0g';
 GRANT ALL PRIVILEGES ON comments.* TO feed_user@localhost;
-
---4c1d90756484d4b6b72c5a3330
