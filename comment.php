b29a9000b8c934ca9d9c6c6abb5df51c4a5e69f6
Lauren Dorval <dorvallg@dukes.jmu.edu>
2021-03-01 16:33:38 -0500

    fsdf

--- comment.php
+++ comment.php
@@ -18,7 +18,7 @@ function getComments() {
   $sql = "SELECT * FROM comment";
   $result = $conn->query($sql);
   while ($row = $result->fetch_assoc()) {
-    echo "<script>console.log(".$row.")</script>";
+    echo "<script>console.log(".$row[u_name].")</script>";
     echo $row['u_name']."<br>";
     echo $row['u_comment']."<br><br>";
   }
