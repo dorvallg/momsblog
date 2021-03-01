9ed6de3b41aac73ee471175cce5dcf469eb9e364
Lauren Dorval <dorvallg@dukes.jmu.edu>
2021-02-28 22:30:31 -0500

    style

--- feed.php
+++ feed.php
@@ -40,13 +40,15 @@
 
         getComments($conn);
         ?>
-    </div>
+        
         <aside id='other-left'>
             <h2>Ask me something!</h2>
             <p>I'm here to help in any ways you may need! Weather that be as a mom figure or a friend.</p>
             <p>Ask about outfits, ideas, life! Anything at all</p>
             <p>Enter your name and email along with what you want toi hear from me about and I will email you back shortly!</p>
         </aside>
+
+    </div>
 
     
 </body>
