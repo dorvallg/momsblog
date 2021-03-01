5fb4782f36d7716a986c088caba4977a4713f286
Lauren Dorval <dorvallg@dukes.jmu.edu>
2021-03-01 13:54:48 -0500

    help

--- databse.php
+++ databse.php
@@ -1,6 +1,11 @@
+<?php
+echo "<script type='text/JavaScript'> 
+
 const mysql = require('mysql');
 
 app.use(express.json());
 
 let table = JSON.parse(fs.readFileSync('credentials.json', 'utf8'));
 let conn = mysql.createConnection(table);
+</script>";
+?>
