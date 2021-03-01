92a8b607e73f549314b9e9e5a6d24e130afabf85
Lauren Dorval <dorvallg@dukes.jmu.edu>
2021-03-01 08:50:13 -0500

    kk

--- comment_database.sql
+++ comment_database.sql
@@ -3,4 +3,4 @@ DROP USER IF EXISTS feed_user@localhost;
 
 CREATE DATABASE comments CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER feed_user@localhost IDENTIFIED WITH mysql_native_password BY 'Laur3nMom$Bl0g';
-GRANT ALL PRIVILEGES ON commentss.* TO feed_user@localhost;
+GRANT ALL PRIVILEGES ON comments.* TO feed_user@localhost;
