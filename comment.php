503a0d3bd7eb9bcd10fdeb8d163e5f6138647954
Lauren Dorval <dorvallg@dukes.jmu.edu>
2021-03-01 14:43:59 -0500

    kjk

--- comment.php
+++ comment.php
@@ -6,11 +6,11 @@ $conn = OpenCon();
 function setComments() {
   if(isset($_POST['commentSubmit'])) {
     echo "here";
-    $u_name = $_POST['u_name'];
-    $u_comment = $_POST['u_comment'];
+    //$u_name = $_POST['u_name'];
+    //$u_comment = $_POST['u_comment'];
 
-    $sql = "INSERT INTO comment(u_name, u_comment) VALUES('$u_name', '$u_comment')";
-    $result = $conn->query($sql);
+    //$sql = "INSERT INTO comment(u_name, u_comment) VALUES('$u_name', '$u_comment')";
+    //$result = $conn->query($sql);
   }
 }
 
